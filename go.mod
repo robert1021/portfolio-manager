45bf5850666d01517bcbe61d73fdec046e0c264e
Robert Sparling <robert.sparling196@gmail.com>
2024-08-17 20:50:34 -0400

    Create db and tables

--- go.mod
+++ go.mod
@@ -5,14 +5,19 @@ go 1.22.0
 require (
 	github.com/gdamore/tcell/v2 v2.7.1
 	github.com/rivo/tview v0.0.0-20240807205129-e4c497cc59ed
+	gorm.io/driver/sqlite v1.5.6
+	gorm.io/gorm v1.25.11
 )
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,70 @@
 package main
 
 import (
+	"os"
+
 	"github.com/rivo/tview"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+// Account represents an account with different currencies
+type Account struct {
+	gorm.Model
+	AccountType string
+	CadBalance  float64
+	UsdBalance  float64
+}
+
+// Stock represents a stock with an associated account
+type Stock struct {
+	gorm.Model
+	Symbol    string  `gorm:"not null"`
+	Average   float64 `gorm:"not null"`
+	Postion   int     `gorm:"not null"`
+	AccountID int     // Foreign key to Account
+	Account   Account `gorm:"foreignKey:AccountID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+// CashTransaction represents a cash transaction linked to an account
+type CashTransaction struct {
+	gorm.Model
+	Amount    float64 `gorm:"not null"`
+	Type      string  `gorm:"not null"`
+	AccountID int     // Foreign key to Account
+	Account   Account `gorm:"foreignKey:AccountID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+// Trade represents a trade transaction linked to an account
+type Trade struct {
+	gorm.Model
+	Symbol    string  `gorm:"not null"`
+	Quantity  float64 `gorm:"not null"`
+	Price     float64 `gorm:"not null"`
+	TradeType string  `gorm:"not null"` // Type of trade: "buy" or "sell"
+	AccountID int     // Foreign key to Account
+	Account   Account `gorm:"foreignKey:AccountID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
 func main() {
+
+	// Set up db
+	if _, err := os.Stat("portfolio-manager.db"); err != nil {
+		db, err := gorm.Open(sqlite.Open("portfolio-manager.db"), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect database")
+		}
+
+		db.AutoMigrate(&Account{})
+		db.AutoMigrate(&Stock{})
+		db.AutoMigrate(&CashTransaction{})
+		db.AutoMigrate(&Trade{})
+
+	}
+
+	// Create
+	// db.Create(&Stock{Symbol: "TSLA", Average: 181, Postion: 100})
+
 	app := tview.NewApplication()
 	pages := tview.NewPages()
 
